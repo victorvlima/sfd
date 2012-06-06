f4bf67ae8e1674041f88cd899cf18539e523adc3
Victor Lima <victor.lima@metrofor.ce.gov.br>
2012-06-06 11:24:37 -0300

    Correção de problema ao reiniciar servidor

--- src/br/gov/ce/metrofor/sfd/util/Fabrica.java
+++ src/br/gov/ce/metrofor/sfd/util/Fabrica.java
@@ -1,8 +1,5 @@
 package br.gov.ce.metrofor.sfd.util;
 
-import java.util.logging.Level;
-import java.util.logging.Logger;
-
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.Persistence;
@@ -15,13 +12,6 @@ public class Fabrica {
 	}
 
 	public static EntityManager getEntityManager() {
-		try {
-			Class.forName("org.postgresql.Driver");
-//			conn = DriverManager.getConnection("jdbc:postgresql://localhost/proc", "proc", "proc");
-//			Statement stmt = conn.createStatement();
-		} catch (ClassNotFoundException ex) {
-			Logger.getLogger(Fabrica.class.getName()).log(Level.SEVERE, null, ex);
-		}
 		if (fabrica == null) {
 			fabrica = Persistence.createEntityManagerFactory("sfd");
 		}
